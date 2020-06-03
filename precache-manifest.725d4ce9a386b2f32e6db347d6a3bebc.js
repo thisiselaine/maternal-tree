ecd9b0a780adcc9e073e9477881aaa69a3af8228
Kiet Ly <kiet.t.ly@gmail.com>
2020-06-03 18:15:32 -0400

    Updates

---
+++ precache-manifest.725d4ce9a386b2f32e6db347d6a3bebc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0560528e5f2bdf4b2783f6fbabf1e313",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c835069b0ae199dfb9e",
+    "url": "/static/css/2.c3343ac3.chunk.css"
+  },
+  {
+    "revision": "63248ee5f992d59e53b3",
+    "url": "/static/css/main.94538993.chunk.css"
+  },
+  {
+    "revision": "7c835069b0ae199dfb9e",
+    "url": "/static/js/2.86ab4f43.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.86ab4f43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63248ee5f992d59e53b3",
+    "url": "/static/js/main.7619045b.chunk.js"
+  },
+  {
+    "revision": "f3738dc735a18e3dd06f",
+    "url": "/static/js/runtime-main.6b6dd63d.js"
+  }
+]);
